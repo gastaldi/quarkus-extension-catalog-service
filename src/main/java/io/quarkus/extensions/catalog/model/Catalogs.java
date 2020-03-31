25d9b5a6a3bf1ddfe34be963defae97cf7c58dc4
George Gastaldi <gegastaldi@gmail.com>
2020-03-31 00:05:56 -0300

    Refactor

--- src/main/java/io/quarkus/extensions/catalog/model/Catalogs.java
+++ src/main/java/io/quarkus/extensions/catalog/model/Catalogs.java
@@ -11,6 +11,8 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -18,27 +20,26 @@ import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logmanager.Logger;
 
 @ApplicationScoped
 public class Catalogs {
 
     private final Map<String, Catalog> catalogs = new HashMap<>();
+    private static final Logger logger = Logger.getLogger(Catalogs.class.getName());
 
     public Catalogs() {
         // Used for testing
     }
 
     /**
-     * Builds a {@link Catalogs} object.
+     * Builds a {@link Catalogs} object
+     *
      * @param subPath must exist and be relative to the user home. Defaults to ~/.quarkus/extension-catalogs
-     * @param objectMapper the JSON parser
-     * @throws IOException if any IO error occurs
+     * @param converter the converter to use
+     * @throws IOException
      */
-    @Inject
-    public Catalogs(
-            @ConfigProperty(name = "quarkus.catalog.path", defaultValue = ".quarkus/extension-catalogs")
-            Path subPath,
-            ObjectMapper objectMapper) throws IOException {
+    public Catalogs(Path subPath, Function<Path, Extension> converter) throws IOException {
         Path catalogPath = Paths.get(System.getProperty("user.home")).resolve(subPath);
         if (!Files.exists(catalogPath)) {
             throw new FileNotFoundException("File not found: " + catalogPath);
@@ -55,22 +56,48 @@ public class Catalogs {
                 return FileVisitResult.CONTINUE;
             }
 
-            @Override public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (file.toString().endsWith(".json")) {
-                    Extension extension = objectMapper.readValue(file.toFile(), Extension.class);
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Extension extension = converter.apply(file);
+                if (extension != null) {
                     catalogBuilder.addExtensions(extension);
                 }
                 return super.visitFile(file, attrs);
             }
 
-            @Override public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
                 put(id, catalogBuilder.build());
                 return FileVisitResult.CONTINUE;
             }
         });
     }
 
-    public void put(String id, Catalog catalog) {
+    /**
+     * Builds a {@link Catalogs} object. This is what CDI uses to create this object
+     * @param subPath must exist and be relative to the user home. Defaults to ~/.quarkus/extension-catalogs
+     * @param objectMapper the JSON parser
+     * @throws IOException if any IO error occurs
+     */
+    @Inject
+    public Catalogs(
+            @ConfigProperty(name = "quarkus.catalog.path", defaultValue = ".quarkus/extension-catalogs")
+                    Path subPath,
+            ObjectMapper objectMapper) throws IOException {
+        this(subPath, path -> {
+                    if (path.toString().endsWith(".json")) {
+                        try {
+                            return objectMapper.readValue(path.toFile(), Extension.class);
+                        } catch (IOException e) {
+                            logger.log(Level.SEVERE, "Error while reading " + path, e);
+                            return null;
+                        }
+                    }
+                    return null;
+        });
+    }
+
+    void put(String id, Catalog catalog) {
         catalogs.put(id, catalog);
     }
 

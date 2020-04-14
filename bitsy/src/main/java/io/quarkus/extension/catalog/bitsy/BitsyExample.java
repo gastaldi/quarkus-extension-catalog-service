56bd7a2eca71ac7c1d860d43e43558d28162dec0
George Gastaldi <gegastaldi@gmail.com>
2020-04-14 12:32:24 -0300

    Fixed kebab tests

---
+++ bitsy/src/main/java/io/quarkus/extension/catalog/bitsy/BitsyExample.java
@@ -0,0 +1,10 @@
+package io.quarkus.extension.catalog.bitsy;
+
+import io.quarkus.extensions.catalog.Indexer;
+
+public class BitsyExample {
+
+    public static void main(String[] args) throws Exception {
+        Indexer indexer = new Indexer();
+    }
+}

5c2cdb86da54ef0ccd915f168a30fb7969a6931d
George Gastaldi <gegastaldi@gmail.com>
2020-06-08 16:20:10 -0300

    Bump JBang class

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.2.0
-//DEPS io.quarkus:quarkus-registry-descriptor:1.0.0.Beta1
+//DEPS io.quarkus:quarkus-registry-descriptor:1.0.0.Beta2
 
 import java.io.File;
 import java.nio.file.Path;

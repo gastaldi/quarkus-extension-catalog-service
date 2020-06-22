f6387d2a339903719815e23c305701f894aeb10d
George Gastaldi <gegastaldi@gmail.com>
2020-06-22 13:27:47 -0300

    Simplified test

--- src/test/java/io/quarkus/registry/builder/ExtensionRegistryBuilderTest.java
+++ src/test/java/io/quarkus/registry/builder/ExtensionRegistryBuilderTest.java
@@ -13,9 +13,9 @@ class ExtensionRegistryBuilderTest {
     @Test
     void shouldResolveCamelDependencies() throws IOException {
         DefaultArtifactResolver resolver = new DefaultArtifactResolver(new ObjectMapper());
-        ExtensionRegistryBuilder builder = new ExtensionRegistryBuilder(resolver);
-        builder.addPlatform("io.quarkus","quarkus-universe-bom","1.5.1.Final");
-        ExtensionRegistry registry = builder.build();
+        ExtensionRegistry registry = new ExtensionRegistryBuilder(resolver)
+                .addPlatform("io.quarkus","quarkus-universe-bom","1.5.1.Final")
+                .build();
 
         Assertions.assertThat(registry.list("1.5.1.Final", "camel")).isNotEmpty();
     }

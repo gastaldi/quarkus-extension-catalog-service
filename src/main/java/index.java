c3d4f44cd62369d16e48dbf34318c4f1faf80955
George Gastaldi <gegastaldi@gmail.com>
2020-07-22 10:03:53 -0300

    Using JBang scripts only

--- src/main/java/index.java
+++ src/main/java/index.java
@@ -1,25 +1,24 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.2.0
-//DEPS io.quarkus:quarkus-registry-descriptor:1.0.0.Beta8
-//DEPS io.quarkus:quarkus-platform-descriptor-json:1.5.1.Final
-
-import java.io.File;
-import java.nio.file.Path;
-import java.util.concurrent.Callable;
+//DEPS io.quarkus:quarkus-platform-descriptor-json:1.6.1.Final
+//DEPS io.quarkus:quarkus-devtools-common:999-SNAPSHOT
+//DEPS io.quarkus:quarkus-registry-descriptor-resolver:1.0.0-SNAPSHOT
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import io.quarkus.registry.RepositoryIndexer;
+import io.quarkus.registry.builder.RegistryBuilder;
 import io.quarkus.registry.catalog.model.Repository;
-import io.quarkus.registry.builder.RegistryModelBuilder;
-import io.quarkus.registry.impl.DefaultArtifactResolver;
+import io.quarkus.registry.resolver.DefaultArtifactResolver;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "index", mixinStandardHelpOptions = true, version = "0.1",
-        description = "Index Catalog")
+@Command(name = "index", mixinStandardHelpOptions = true, version = "0.1", description = "Index Catalog")
 class index implements Callable<Integer> {
 
     @Parameters(index = "0", description = "The repository path to index")
@@ -39,8 +38,8 @@ class index implements Callable<Integer> {
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .setPropertyNamingStrategy(PropertyNamingStrategy.KEBAB_CASE);
         Repository repository = Repository.parse(repositoryPath, mapper);
-        RepositoryIndexer indexer = new RepositoryIndexer(new DefaultArtifactResolver(mapper));
-        RegistryModelBuilder builder = new RegistryModelBuilder();
+        RepositoryIndexer indexer = new RepositoryIndexer(new DefaultArtifactResolver());
+        RegistryBuilder builder = new RegistryBuilder();
         indexer.index(repository, builder);
         // Make sure the parent directory exists
         File parentFile = outputFile.getAbsoluteFile().getParentFile();
--- src/main/java/io/quarkus/registry/package-info.java
+++
@@ -1,6 +0,0 @@
-@Style(jdkOnly = true, visibility = PRIVATE, builder = "new", allowedClasspathAnnotations = {Override.class})
-package io.quarkus.registry;
-
-import org.immutables.value.Value.Style;
-
-import static org.immutables.value.Value.Style.ImplementationVisibility.PRIVATE;
